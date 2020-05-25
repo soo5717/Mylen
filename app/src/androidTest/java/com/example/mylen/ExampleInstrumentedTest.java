f12b61f13da890807d246131bdb05906d68c11ad
expresshighway <03wow22@gmail.com>
2020-05-25 23:59:59 +0900

    yg-test

--- app/src/androidTest/java/com/example/mylen/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mylen/ExampleInstrumentedTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
+ * yg test
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
