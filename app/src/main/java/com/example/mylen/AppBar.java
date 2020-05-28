ad96af31952ec80ae44792dfc1d18a2a9e9fe5fa
expresshighway <03wow22@gmail.com>
2020-05-29 00:39:10 +0900

    yg -version 3: AppBar

--- app/src/main/java/com/example/mylen/AppBar.java
+++ app/src/main/java/com/example/mylen/AppBar.java
@@ -1,6 +1,8 @@
 package com.example.mylen;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,12 @@ public class AppBar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_bar);
+
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+    }
+
 }
