2cacf77b3119f31684f31e204248a97099a3e270
suyeon <koo5717@naver.com>
2020-05-28 09:31:43 +0900

    search_lens

--- app/src/main/java/com/example/mylen/home/search/SearchLensActivity.java
+++ app/src/main/java/com/example/mylen/home/search/SearchLensActivity.java
@@ -9,6 +9,7 @@ import com.example.mylen.R;
 public class SearchLensActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_lens);
