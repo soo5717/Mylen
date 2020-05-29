819183493dc394de322b3a42702a151514b6a340
expresshighway <03wow22@gmail.com>
2020-05-29 20:53:27 +0900

    yg -version 8: add splash completed

---
+++ app/src/main/java/com/example/mylen/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.mylen;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private Object MainActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("state","launch");
+        startActivity(intent);
+        finish();
+    }
+}
