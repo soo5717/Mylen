a6597b23d2ab085a9b48450422767c81eeb4f794
baekseoyoon <bsj8993@naver.com>
2020-06-01 14:07:25 +0900

    sy-new

---
+++ app/src/main/java/com/example/mylen/home/modify/Modifyliquid.java
@@ -0,0 +1,16 @@
+package com.example.mylen.home.modify;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mylen.R;
+
+public class Modifyliquid extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifyliquid1);
+        setContentView(R.layout.activity_modifyliquid2);
+
+    }
+}
