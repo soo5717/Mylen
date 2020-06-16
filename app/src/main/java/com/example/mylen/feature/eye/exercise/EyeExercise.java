f42d26eec8d59ef3c261a55c19ec4212671d2d62
expresshighway <03wow22@gmail.com>
2020-06-16 17:01:43 +0900

    yg version 25: add EyeExercise

---
+++ app/src/main/java/com/example/mylen/feature/eye/exercise/EyeExercise.java
@@ -0,0 +1,16 @@
+package com.example.mylen.feature.eye.exercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mylen.R;
+
+public class EyeExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eye_exercise);
+    }
+}
