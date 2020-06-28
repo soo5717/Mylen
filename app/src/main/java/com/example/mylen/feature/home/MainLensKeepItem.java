d4ca943af75270d5f06829c44aa049ab950c1d9b
suyeon <koo5717@naver.com>
2020-06-28 16:05:25 +0900

    delete main items

--- app/src/main/java/com/example/mylen/feature/home/MainLensKeepItem.java
+++
@@ -1,7 +0,0 @@
-package com.example.mylen.feature.home;
-
-
-
-public class MainLensKeepItem {
-
-}
--- app/src/main/java/com/example/mylen/feature/home/adapter/LiquidKeepAdapter.java
+++ app/src/main/java/com/example/mylen/feature/home/adapter/LiquidKeepAdapter.java
@@ -124,4 +124,6 @@ public class LiquidKeepAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             return "";
         }
     }
+
+    //
 }
